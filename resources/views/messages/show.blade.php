331543cd5ca8ea8162e92ca5e16199006c64c312
log1kuma <log1@gmail.com>
2018-05-25 03:41:24 +0000

    bootstrap design

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -2,14 +2,25 @@
 
 @section('content')
 
-    <h1>id = {{ $message->id }} のメッセージ詳細ページ</h1>
-    <h3>タイトル : {{ $message->title }}</h3>
-    <p>メッセージ : {{ $message->content }}</p>
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $message->id }}</td>
+        </tr>
+        <tr>
+            <th>タイトル</th>
+            <td>{{ $message->title }}</td>
+        </tr>
+        <tr>
+            <th>メッセージ</th>
+            <td>{{ $message->content }}</td>
+        </tr>
+    </table>
     
-    {!! link_to_route('messages.edit', 'このメッセージを編集', ['id' => $message->id]) !!}
+    {!! link_to_route('messages.edit', 'このメッセージを編集', ['id' => $message->id], ['class' => 'btn btn-default']) !!}
     
     {!! Form::model($message, ['route' => ['messages.destroy', $message->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 
 @endsection
