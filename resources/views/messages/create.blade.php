595a21b47c4807fef5ef63e2117ca7252f332662
log1kuma <log1@gmail.com>
2018-06-04 07:24:14 +0000

    登録がうまくいったら、成功しました！と表示

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 
     <h1>メッセージ新規作成ページ</h1>
+    <div id="alert"></div>
 
        <div class="row">
         <div class="col-xs-6">
@@ -35,11 +36,15 @@
 /*global $*/
 
     // 成功したらやる処理
-    function onFetchComplete(){
+    function onFetchComplete(response){
         console.log("わーい");
+        
         // フォームの中をクリアする
         $("#title").val('');
         $("#content").val('');
+        
+        // 成功しましたアラートを出す
+        $('#alert').addClass('alert alert-success').attr('role', 'alert').text(response).show();
     }
 
     
@@ -52,12 +57,17 @@
         
         // sendFormのsubmitボタンが押されたら
         $("#sendForm").submit(function(event){
-
+        
+        // ページ遷移を阻止
         event.preventDefault();
+        
+        // 入力された文字を変数に代入
         var title = $("#title").val();
-        console.log(title);
         var content = $("#content").val();
-        console.log(content);
+        
+        // 連続で投稿ボタン押した時用
+        // 投稿成功メッセージを隠しておく
+        $('#alert').hide();
 
         // ２重送信防止処理
         // 送信ボタンを押せないようにする
@@ -86,7 +96,7 @@
             // falseを返すと処理を中断
                 console.log("beforeSend");
                 
-                // 3秒後に、ボタンを押せるようにする
+                // 3秒後に、送信ボタンを押せるようにする
                 setTimeout(function(){
                     btn.prop('disabled', false);
                 }, 3000)
@@ -95,7 +105,8 @@
                 img.show();
                 
                 $('.form-group').removeClass('has-error');
-                $('.form-group').children('.help-block').hide();
+                $('.form-group').children('.help-block').text('');
+                // $('.form-group').children('.help-block').hide();
                 return true;
             },
                 // コールバックにthisで参照させる要素(DOMなど)
@@ -103,32 +114,30 @@
               }).done(function(response, textStatus, jqXHR) {
                 // 成功時処理
                 //レスポンスデータはパースされた上でresponseに渡される
-                console.log("done");
-                console.log(response.return);
-                console.log(response.message);
                 onFetchComplete(response.return);
               }).fail(function(jqXHR, textStatus, errorThrown ) {
                 // 失敗時処理
-                console.log("errorだよ");
-                console.log(jqXHR.responseJSON);
+                
+                // エラーが422(バリデーション)だったら
                 if(jqXHR.status === 422){
                     var errors = jqXHR.responseJSON.errors;
                     
                     for(var key in errors){
                         var error_id = '#' + key;
-                        // var span = $('<span>');
                         // error_idの親クラスの.form-groupにhas-errorを追加する
                         $(error_id).parent('.form-group').addClass('has-error');
-                        $(error_id).next('.help-block').show();
+                        // $(error_id).next('.help-block').show();
                         $(error_id).next('.help-block').text(errors[key]);
                     }
                     
                     // 422エラーなら、すぐにボタンを押せるようにする
                     btn.prop('disabled', false);
                     
+                    // $('#alert').removeClass('alert-success').addClass('alert alert-danger').attr('role', 'alert').text('失敗しました。').show();
+                    
                 }else{
                     // 422以外
-                    $('#sendForm').append('<p>失敗しました</p>');
+                    $('#alert').removeClass('alert-success').addClass('alert alert-danger').attr('role', 'alert').text('失敗しました。').show();
                 }
                 
               }).always(function(data_or_jqXHR, textStatus, jqXHR_or_errorThrown) {
